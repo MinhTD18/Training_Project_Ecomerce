6cf9e1b7331034e05c78c855f62f525d6df3b137
Minh Truong <truongducminh1997@gmail.com>
2021-01-02 21:23:34 +0700

    (feat)config template

--- store/urls.py
+++ store/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.store, name='store'),
     path('cart/', views.cart, name='cart'),
     path('checkout/', views.checkout, name='checkout'),
-    path('register/', views.get_register_api, name='register')
-    path('login/', views.login, name='login')
+    path('register/', views.get_register_api, name='register'),
+    path('login/', views.login, name='login'),
 ]
