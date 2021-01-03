37ee7bfd5ae80a54945f8c0072ec1108dcb37f74
Minh Truong <truongducminh1997@gmail.com>
2021-01-04 05:21:29 +0700

    (feat)direct main html

--- store/urls.py
+++ store/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.store, name='main'),
+    path('', views.main, name='main'),
     path('cart/', views.cart, name='cart'),
     path('checkout/', views.checkout, name='checkout'),
     path('login/', views.login, name='login'),
