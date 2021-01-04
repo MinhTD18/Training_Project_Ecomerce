3adb86abe349e8b091ba67f6dad92c965c86cfad
Minh Truong <truongducminh1997@gmail.com>
2021-01-04 11:25:24 +0700

    (feat)test deploy

--- Training_Project_Ecomerce/settings.py
+++ Training_Project_Ecomerce/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'Training_Project_Ecomerce.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
